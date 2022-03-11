b104f99a77a0660fd01e2556d41a1992c753e1a9
Nikita1814 <kravchenko1814@gmail.com>
2022-03-11 15:22:51 +0300

    feat: initialize app install lint and create basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchItemComponent } from './search-results/search-item/search-item.component';
+import { FilteringCriteriaComponent } from './filtering-criteria/filtering-criteria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    FilteringCriteriaComponent
   ],
   imports: [
     BrowserModule,
