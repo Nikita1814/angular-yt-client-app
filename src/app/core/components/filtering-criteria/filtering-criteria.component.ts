38d1f0de16fa59cb1367c3f0f1305818bc9ccf9d
Nikita1814 <kravchenko1814@gmail.com>
2022-03-29 19:26:27 +0300

    refactor: move sorting functions to a separate util file

--- src/app/core/components/filtering-criteria/filtering-criteria.component.ts
+++ src/app/core/components/filtering-criteria/filtering-criteria.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ResponseManagementService } from '../../../youtube/services/response-management.service';
 import { ResponseVidInt } from '../../../youtube/models/response-vid-model';
+import sortFuncs from '../../utils/sort-funcs';
 
 @Component({
   selector: 'app-filtering-criteria',
@@ -12,18 +13,7 @@ export class FilteringCriteriaComponent implements OnInit {
     [key: string]: (a: ResponseVidInt, b: ResponseVidInt) => number;
   };
   constructor(public respService: ResponseManagementService) {
-    this.sortFuncs = {
-      date: (a: ResponseVidInt, b: ResponseVidInt): number => {
-        const dOne = new Date(a.snippet.publishedAt).getTime();
-        const dTwo = new Date(b.snippet.publishedAt).getTime();
-        return dOne > dTwo ? 1 : dOne < dTwo ? -1 : 0;
-      },
-      views: (a: ResponseVidInt, b: ResponseVidInt): number => {
-        const v1 = a.statistics.viewCount;
-        const v2 = b.statistics.viewCount;
-        return v1 > v2 ? 1 : v1 < v2 ? -1 : 0;
-      },
-    };
+    this.sortFuncs = sortFuncs;
   }
 
   ngOnInit(): void {}
